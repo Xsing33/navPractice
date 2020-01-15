d3ebbd6820771d8956554f3d6627318b51931192
Xavier Singletary <52980318+Xsing33@users.noreply.github.com>
2020-01-15 00:40:23 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+// const head = document.getElementById("header");
+// let lastScrolee
+
+// header.addEventListener("scroll", opacityHeader);
+
+// function opacityHeader() {
+//   if
+// }
 const header = document.querySelector("header");
+const sectionOne = document.querySelector(".about");
 
-header.addEventListener("scroll", opacityHeader);
+const sectionOneOptions = {
+  rootMargin: "0px 0px 0px 0px"
+};
 
-function opacityHeader() {
-  header.onscroll.style.opacity = 1;
-}
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+sectionOneObserver.observe(sectionOne);
